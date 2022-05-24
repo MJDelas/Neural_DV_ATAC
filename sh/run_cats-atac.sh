8fe517380e757c63501df835e5e3d1bc9ee253ca
MJDelas <joaquina.delas@crick.ac.uk>
2022-05-24 10:22:02 +0100

    Create run_cats-atac.sh

---
+++ sh/run_cats-atac.sh
@@ -0,0 +1,23 @@
+export NXF_WORK="/camp/svc/scratch/briscoej/joaquina/nf-core_atacseq"
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/20.01.0
+ml Singularity/2.6.0-foss-2016b
+
+
+## UPDATE PIPELINE
+nextflow pull nf-core/atacseq
+
+## RUN PIPELINE
+nextflow run nf-core/atacseq \
+    --input  design_filtered2.csv \
+    --genome mm10 \
+    --gtf '/camp/lab/briscoej/working/Joaquina/hpc_camp/delasj_reference_files//mm10_gtf_noheader/mm10.refGene.gtf' \
+    --macs_fdr 0.00001 \
+    --skip_diff_analysis \
+    --min_reps_consensus 2 \
+    --email joaquina.delas@crick.ac.uk \
+    -profile crick \
+    -r 1.2.0 \
+    -resume
